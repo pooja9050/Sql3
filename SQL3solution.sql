12f2f41868870ffbae1cef4de4361d574a0b520d
Pooja Madhup <90482825+pooja9050@users.noreply.github.com>
2024-05-04 07:13:07 -0700

    Update SQL3solution.sql

--- SQL3solution.sql
+++ SQL3solution.sql
@@ -95,4 +95,17 @@ FROM CTE WHERE experience = 'Junior' AND rsum <=(
 );
 
 
+# If hire Either senior or junior using more than one common table expression cte
+With CTE AS(
+    SELECT employee_id, experience, SUM(salary) OVER(PARTITION BY experience ORDER BY salary) AS 'rsum' FROM Candidates
+),
+ACTE AS (
+    SELECT 70000-IFNULL(MAX(rsum),0) AS 'remaining' FROM CTE WHERE experience = 'Senior' AND rsum <=70000
+)
+
+SELECT 'Senior' AS experience , COUNT(employee_id) AS accepted_candidates FROM CTE
+WHERE experience = 'Senior' AND rsum <= 70000
+UNION
+SELECT 'Junior' AS experience, COUNT(employee_id) AS accepted_candidates FROM CTE
+Where experience = 'Junior' AND rsum <= (SELECT remaining FROM ACTE);
 
